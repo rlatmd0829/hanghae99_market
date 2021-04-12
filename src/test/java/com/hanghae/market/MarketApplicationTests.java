054ba938b4f595a88c34d802a8d0e3b79825f394
seungwook <rlatmd0829@naver.com>
2021-04-12 10:16:27 +0900

    Initial commit

---
+++ src/test/java/com/hanghae/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

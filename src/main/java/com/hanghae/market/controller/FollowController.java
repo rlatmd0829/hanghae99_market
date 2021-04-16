d8861aec2adc18fe7af4ec59b4e37278ee8acb47
seungwook <rlatmd0829@naver.com>
2021-04-16 14:30:07 +0900

    2021.04.16 14:30 검색, 게시글 수정 사진넣어서 구현, 주석 추가

--- src/main/java/com/hanghae/market/controller/FollowController.java
+++ src/main/java/com/hanghae/market/controller/FollowController.java
@@ -4,6 +4,7 @@ import com.hanghae.market.config.auth.PrincipalDetails;
 import com.hanghae.market.model.Follow;
 import com.hanghae.market.model.Message;
 import com.hanghae.market.service.FollowService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,20 +13,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
+@RequiredArgsConstructor
 public class FollowController {
 
-    final
-    FollowService followService;
+    private final FollowService followService;
 
-    public FollowController(FollowService followService) {
-        this.followService = followService;
-    }
-
+    // 팔로우 조회(팔로우 상태, 상대 팔로우 횟수)
     @GetMapping("/users/{followUserId}/follow")
     public HashMap<String, Object> getFollow(@PathVariable Long followUserId, @AuthenticationPrincipal PrincipalDetails userDetails){
         return followService.getFollow(followUserId, userDetails.getUser().getId());
     }
 
+    // 팔로우 하기
     @PostMapping("/users/{followUserId}/follow")
     public ResponseEntity createFollow(@PathVariable Long followUserId, @AuthenticationPrincipal PrincipalDetails userDetails){
         if (followUserId.equals(userDetails.getUser().getId())){
@@ -40,6 +39,7 @@ public class FollowController {
         return ResponseEntity.ok().build();
     }
 
+    // 팔로우 취소
     @DeleteMapping("/users/{followUserId}/follow")
     public ResponseEntity deleteFollow(@PathVariable Long followUserId, @AuthenticationPrincipal PrincipalDetails userDetails){
         Follow follow = followService.deleteFollow(followUserId, userDetails.getUser().getId());
--- src/main/java/com/hanghae/market/service/BoardService.java
+++ src/main/java/com/hanghae/market/service/BoardService.java
@@ -30,31 +30,60 @@ public class BoardService {
         return mainDtoList;
     }
 
+    public List<BoardMainDto> getSearchBoard(String title) {
+        List<Board> board = boardRepository.findByTitleContainingOrContentContaining(title, title);
+        List<BoardMainDto> mainDtoList = new ArrayList<>();
+        // main에 필요한 값들만 Dto로 만들어서 보내준다.
+        for(int i=0; i<board.size(); i++){
+            BoardMainDto mainDto = new BoardMainDto(board.get(i));
+            mainDtoList.add(mainDto);
+        }
+        return mainDtoList;
+    }
+
     public void createBoard(BoardRequestDto requestDto, Long userId) {
         User user = userRepository.findById(userId).orElseThrow(
                 () -> new IllegalArgumentException("계정이 존재하지 않습니다.")
         );
         Board board = new Board(requestDto);
-        board.addUser(user);
+        board.addUser(user); // 연관관계 추가 (댓글 만들어지면 또 댓글도 추가해야함)
         boardRepository.save(board);
 
     }
 
     @Transactional
-    public Board updateBoard(Long boardId, BoardRequestDto requestDto) {
+    public Board updateBoard(Long boardId, BoardRequestDto requestDto, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("계정이 존재하지 않습니다.")
+        );
         Board board = boardRepository.findById(boardId).orElseThrow(
                 ()-> new IllegalArgumentException("게시글이 존재하지 않습니다.")
         );
-        board.update(requestDto);
-        return board;
+        if (board.getUser().getId().equals(userId)){
+            board.update(requestDto);
+            return board;
+        }
+        else{
+            return null;
+        }
+
     }
 
     @Transactional
-    public void deleteBoard(Long boardId) {
+    public Board deleteBoard(Long boardId, Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("계정이 존재하지 않습니다.")
+        );
         Board board = boardRepository.findById(boardId).orElseThrow(
                 ()-> new IllegalArgumentException("게시글이 존재하지 않습니다.")
         );
-        boardRepository.deleteById(boardId);
+        if (board.getUser().getId().equals(userId)) {
+            boardRepository.deleteById(boardId);
+            return board;
+        }
+        else{
+            return null;
+        }
     }
 
 
@@ -64,4 +93,6 @@ public class BoardService {
         );
         return board;
     }
+
+
 }
